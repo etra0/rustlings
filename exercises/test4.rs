eae5c0ec5864860f904579724fa6e64c2bf2c114
Sebastián Aedo <saedo@alumnos.inf.utfsm.cl>
2020-08-25 04:44:29 -0400

    Some progress on rustlings

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,7 +5,12 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+
+macro_rules! my_macro {
+    ($var:expr) => {
+        format!("Hello {}", $var);
+    }
+}
 
 #[cfg(test)]
 mod tests {
